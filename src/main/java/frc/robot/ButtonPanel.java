e9da7be448997800e5591a22b733c0f3192890b6
github3658 <github@team3658.com>
2025-02-16 14:51:00 -0500

    Removed redundant files, standardized most naming conventions, and created default commands for elevator and end effector (subsystems still need to be implemented)

---
+++ src/main/java/frc/robot/ButtonPanel.java
@@ -0,0 +1,58 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+
+public class ButtonPanel {
+    public enum Button {
+        Stow(1, 1),
+        ElevatorPosition2(2, 2),
+        ElevatorPosition3(3, 3),
+        ElevatorPosition4(4, 4),
+        ElevatorPositionBarge(5, 5),
+        ManualClaw1(6),
+        ManualClaw2(7),
+        ClawPositionCoral(8, 6),
+        ClawPositionAlgae(9, 7),
+        CoralIn(10, 8),
+        CoralOut(11, 9),
+        AlgaeIn(12, 10),
+        AlgaeOut(13, 11),
+        ShallowClimb(14),
+        DeepClimbGrab(15),
+        DeepClimbClimb(16);
+        public final int value;
+        public final int indicator;
+        Button(int value) {
+            this.value = value;
+            this.indicator = 0;
+        }
+        Button(int value, int indicator) {
+            this.value = value;
+            this.indicator = indicator;
+        }
+    }
+
+    private GenericHID button_panel;
+
+    public ButtonPanel(int port) {
+        button_panel = new GenericHID(port);
+    }
+
+    /**
+     * Returns whether a button is held or not.
+     * @param button The button to check
+     * @return True or false
+     */
+    public boolean getButton(Button button) {
+        return button_panel.getRawButton(button.value);
+    }
+
+    /**
+     * Returns whether a button has just been pressed.
+     * @param button The button to check
+     * @return True or false
+     */
+    public boolean getButtonPressed(Button button) {
+        return button_panel.getRawButtonPressed(button.value);
+    }
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,22 +4,46 @@
 
 package frc.robot;
 
+import frc.robot.subsystems.*; // This imports all of our subsystems.
+import frc.robot.ButtonPanel.Button;
+import frc.robot.commands.*; // This imports all of our commands.
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.generated.TunerConstants;
 
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+ 
+  // This is the driver's controller.
+  // Typing "xb_Driver." will show you all of the XboxController methods, such as querying button inputs.
+  private XboxController xb_Driver = new XboxController(0);
 
-  private final RobotContainer m_robotContainer;
+  // This is the operator's button panel.
+  // This is a custom class I made that lets you check for input using a Button enum.
+  // private ButtonPanel bp_Operator = new ButtonPanel(1);
 
-  public Robot() {
-    m_robotContainer = new RobotContainer();
+  private CommandSwerveDrivetrain s_Swerve = TunerConstants.createDrivetrain();
+  //private Elevator s_Elevator = new Elevator();
+  //private SameDayDelivery s_EndEffector = new SameDayDelivery();
+
+  @Override
+  public void robotInit() {
+    s_Swerve.setDefaultCommand(new SwerveDrive(s_Swerve, xb_Driver));
+    //s_Elevator.setDefaultCommand(new ElevatorDefault(s_Elevator, bp_Operator));
+    //s_EndEffector.setDefaultCommand(new EndEffectorDefault(s_EndEffector, bp_Operator, s_Elevator));
   }
 
   @Override
   public void robotPeriodic() {
-    CommandScheduler.getInstance().run(); 
+    CommandScheduler.getInstance().run();
+    SmartDashboard.putNumber("Limelight Target X", LimelightHelpers.getTX(""));
+    //SmartDashboard.putBoolean("Stow Button", bp_Operator.getButton(Button.Stow));
   }
 
   @Override
@@ -33,11 +57,43 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    // We'll count the reef faces from 1 to 6
 
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
+    // REEF FACE 1
+    // new SequentialCommandGroup(
+		//   new DriveToPose(s_Swerve, new Pose2d(new Translation2d(3, 0), new Rotation2d()))
+		// ).schedule();
+
+    // FACE 6
+    // X = 4
+    // Y = -1.6
+    // ROT = 58
+
+    // FACE 5
+    // X = 6
+    // Y = -1.4
+    // ROT = 135
+
+    // FACE 4
+    // X = 6.3
+    // Y = 0
+    // ROT = 180
+
+    // FACE 3
+    // X = 5.5
+    // Y = 1.35
+    // ROT = -118.6
+
+    // FACE 2
+    // X = 3.95
+    // Y = 1.2
+    // ROT = -60
+    new SequentialCommandGroup(
+		  new DriveToPose(s_Swerve, new Pose2d(new Translation2d(1.5, 0), new Rotation2d())),
+		  new DriveToPose(s_Swerve, new Pose2d(new Translation2d(3.95, 1.5), new Rotation2d(Math.toRadians(-60)))),
+		  new DriveToPose(s_Swerve, new Pose2d(new Translation2d(3.95, 1.2), new Rotation2d(Math.toRadians(-60))))
+		).schedule();
+    //new Orangelight(s_Swerve).schedule();
   }
 
   @Override
@@ -48,14 +104,14 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
   }
 
   @Override
-  public void teleopPeriodic() {}
-
+  public void teleopPeriodic() {
+    if (xb_Driver.getStartButtonPressed()) {
+      s_Swerve.resetPose(new Pose2d());
+    }
+  }
   @Override
   public void teleopExit() {}
 
